66ae4604153789077bf5d19e80431882c7f48db2
Charlie Choi <chaizi@Charlies-MacBook-Pro-6.local>
2022-03-20 18:52:17 +0800

    ADDS prac3 change5

--- Crescendo.cpp
+++ Crescendo.cpp
@@ -8,7 +8,8 @@ Crescendo::Crescendo()
 char Crescendo::makeMove()
 {
     char move_array[3]={'R','P','S'};
-    int count=count%3;
+    int count=0;
+    count=count%3;
     count++;
     return move_array[count-1];
 }
--- FistfullODollars.cpp
+++ FistfullODollars.cpp
@@ -8,7 +8,8 @@ FistfullODollars::FistfullODollars()
 char FistfullODollars::makeMove()
 {
     char move_array[3]={'R','P','P'};
-    int i=i%3;
+    int i=0;
+    i=i%3;
     i++;
     return move_array[i-1];
 }
