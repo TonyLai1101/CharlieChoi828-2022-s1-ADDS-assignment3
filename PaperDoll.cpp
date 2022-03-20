218a57b076057beebc7a12fd0690da910d89a305
Charlie Choi <chaizi@Charlies-MacBook-Pro-6.local>
2022-03-20 18:53:49 +0800

    ADDS prac3 change 6

--- PaperDoll.cpp
+++ PaperDoll.cpp
@@ -8,7 +8,8 @@ PaperDoll::PaperDoll()
 char PaperDoll::makeMove()
 {
     char move_array[3]={'P','S','S'};
-    int count=count%3;
+    int count=0;
+    count=count%3;
     count++;
     return move_array[count-1];
 }
